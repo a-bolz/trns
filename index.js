63e94e1e29768a567efa32ccb8a11df377524ec0
Andreas Bolz <andreasbolz@gmail.com>
2018-10-18 21:26:57 +0200

    added dotenv, added gitignore

--- index.js
+++ index.js
@@ -3,14 +3,32 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const bodyParser = require('body-parser');
 
+require('dotenv').config()
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .use(bodyParser.json())
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/authenticate', (req, res) => res.render('pages/authenticate'))
   .post('/onzeapi', (req, res) => {
     console.log(req.body)
     res.send('thanks')
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+//Webhook Post Data
+//{
+//  type: 'deal.moved',
+//  subject: {
+//    type: 'deal', 
+//    id: '60592a1b-8c7d-017e-a161-feaee35686f7' 
+//  },
+//  account: {
+//    type: 'account', 
+//    id: '0831327a-9e1a-0e3c-a85f-f3b3b911d0e8' 
+//  }
+//}
+
