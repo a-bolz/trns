600dc61af527359a398381c68b242a5a4cbce6e6
Andreas Bolz <andreasbolz@gmail.com>
2018-10-18 22:20:10 +0200

    added useless user class

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
+require User from './user'
+
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const OAuth2Strategy = require('passport-oauth2');
+
+
 
 require('dotenv').config()
+
+passport.use(new OAuth2Strategy({
+  authorizationURL: 'https://app.teamleader.eu/oauth2/authorize',
+  tokenURL: 'https://app.teamleader.eu/oauth2/access_token',
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: "http://localhost:5000/auth/example/callback",
+},
+function(accessToken, refreshToken, profile, cb) {
+  console.log(accessToken, '\n\n', refreshToken, '\n\n', profile, '\n\n', cb)
+//  User.findOrCreate({ exampleId: profile.id }, function (err, user) {
+//    return cb(err, user);
+//  });
+}
+));
+
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .use(bodyParser.json())
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+  .get('/auth/example', passport.authenticate('oauth2'))
+  .get(
+    '/auth/example/callback', 
+    passport.authenticate('oauth2', { failureRedirect: '/login' }),
+    function(req, res) {
+      // Successful authentication, redirect home.
+      res.redirect('/');
+   })
   .get('/', (req, res) => res.render('pages/index'))
   .get('/authenticate', (req, res) => res.render('pages/authenticate'))
   .post('/onzeapi', (req, res) => {
