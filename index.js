1eeac23fbcd725f9fdcb6a87ad88c9bcc4ad535a
Andreas Bolz <andreasbolz@gmail.com>
2018-10-18 19:38:37 +0200

    werkende body parser

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const bodyParser = require('body-parser');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .use(bodyParser)
+  .use(bodyParser.json())
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .post('/onzeapi', (req, res) => {
     console.log(req.body)
+    res.send('thanks')
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
