d1b72b60365a2b3cdec54f70f62bd4657c11ec7f
Andreas Bolz <andreasbolz@gmail.com>
2018-10-22 12:07:41 +0200

    authenticatie nu zonder passport, en werkend

--- index.js
+++ index.js
@@ -5,73 +5,75 @@ const PORT = process.env.PORT || 5000
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const OAuth2Strategy = require('passport-oauth2');
+const mongoose = require('mongoose')
 const mongodb = require("mongodb");
 const objectID = mongodb.ObjectID;
+const calls = require('./services/calls')
+const url = require('url')
+const axios = require('axios')
+const router = express.Router()
 
-
-passport.use(new OAuth2Strategy({
-  authorizationURL: 'https://app.teamleader.eu/oauth2/authorize',
-  tokenURL: 'https://app.teamleader.eu/oauth2/access_token',
-  clientID: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: "http://localhost:5000/auth/example/callback",
-},
-  function(accessToken, refreshToken, profile, cb) {
-    console.log(accessToken, '\n\n', refreshToken, '\n\n', profile, '\n\n', cb)
-  //  User.findOrCreate({ exampleId: profile.id }, function (err, user) {
-  //    return cb(err, user);
-  //  });
-  }
-));
 
 const app = express()
 
-
-var db;
-
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/textwerk_dev", function (err, client) {
-  if (err) {
-    process.exit(1);
-  }
-
-  //save database object from the callback for reuse
-  db = client.db();
-  console.log("Database connection ready");
-
-
-  //initialize the app.
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/textwerk_dev")
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'))
+db.once('open', function() {
+  console.log('db connection established')
   const server = app.listen(process.env.PORT || 5000, function () {
     const port = server.address().port;
     console.log("App now running on port", port);
   });
-});
+})
 
 app.use(express.static(path.join(__dirname, 'public')))
-.use(bodyParser.json())
-.set('views', path.join(__dirname, 'views'))
-.set('view engine', 'ejs')
-.get('/auth/example', passport.authenticate('oauth2'))
-.get(
-  '/auth/example/callback', 
-  passport.authenticate('oauth2', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/');
- })
-.get('/', (req, res) => res.render('pages/index'))
-.get('/authenticate', (req, res) => res.render('pages/authenticate'))
-.post('/onzeapi', (req, res) => {
-  res.send('thanks')
-})
-.get('/milan', (req, res) => {
-  db.collection('milan').find({}).toArray(function(err, docs) {
-    if (err) {
-      console.log(err) 
-    } else {
-      res.status(200).json(docs)
-    }
+  .use(bodyParser.json())
+  .use(passport.initialize())
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/auth/example', (req, res) => {
+    res.redirect(url.format({
+      pathname: "https://app.teamleader.eu/oauth2/authorize",
+      query: {
+        client_id: process.env.CLIENT_ID,
+        response_type: 'code',
+        redirect_uri: process.env.REDIRECT_URI || 'http://localhost:5000/auth/example/callback',
+      }
+    }));
   })
-})
+  .get('/auth/example/callback', (req, res) => {
+    axios({
+      method: 'post',
+      url: 'https://app.teamleader.eu/oauth2/access_token',
+      headers: {'content-type': 'application/json'},
+      data: {
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        redirect_uri: process.env.REDIRECT_URI || 'http://localhost:5000/auth/example/callback',
+        code: req.query.code,
+        grant_type: 'authorization_code'
+      }
+    }).then((response) => {
+      db.collection('tokens').drop()
+      db.collection('tokens').insert({ 
+        token_type: response.data.token_type, 
+        expires_in: response.data.expires_in, 
+        access_token: response.data.access_token, 
+        refresh_token: response.data.refresh_token })
+      res.send(`authentication ok: access_token: ${response.data.access_token}, refresh_token: ${response.data.refresh_token}`)
+    }).catch((error) => {
+      console.log(error)
+    })
+  })
+  .get('/see_latest', (req, res) => {
+    
+  })
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/authenticate', (req, res) => res.render('pages/authenticate'))
+  .post('/onzeapi', (req, res) => {
+    res.send('thanks')
+  })
 
 //Webhook Post Data
 //{
